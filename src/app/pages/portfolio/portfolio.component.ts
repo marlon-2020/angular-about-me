5ff05b4c26d8b9541665610e8faf3029e4932498
Marlon Silva Ferreira <71356608+marlon-2020@users.noreply.github.com>
2023-12-20 18:27:44 -0300

    9th commit, adiciona melhorias na tela do portfolio, adiciona melhorias no menu side bar, adiciona json de portfolio para injeção de dados

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.sass']
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
+
+  constructor(private http: HttpClient){
+
+  }
+
+  jsonUrl = "./assets/portfolio.json"
+
+  ngOnInit(): void {
+    this.http.get(`${this.jsonUrl}`).subscribe((respsonse: any)=>{
+      this.portfolioList = respsonse.portfolio
+      console.log(this.portfolioList)
+    })
+  }
+
+  portfolioList: any[] = []
+
+
 
 }
